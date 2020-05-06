2a628187c87125a8a2898500c13de2fb12d95c7f
shuya-tamaru <tamaru1013@gmail.com>
2020-05-06 16:52:10 +0900

    initial_commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <LikeHeader>
+      <h3>初めまして</h3>
+    </LikeHeader>
+    <LikeNumber :total-number='number' @my-click="incrementNumber"></LikeNumber>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <keep-alive>
+      <component :is="currentComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LikeHeader from "./components/LikeHeader.vue";
+import About from "./components/About.vue";
+import Home from "./components/Home.vue";
+
 
 export default {
-  name: 'App',
+  data() {
+    return{
+      number: 14,
+      currentComponent: 'Home'
+    };
+  },
   components: {
-    HelloWorld
+    LikeHeader,
+    About,
+    Home
+  },
+  methods: {
+    incrementNumber(value){
+      this.number = value;
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style scoped> 
+  div {
+    border:1px solid blue
+  }
+/* 
+  h1 {
+    color:red;
+  } */
 </style>
---
+++ src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <p>About</p>
+  <input>
+</div>
+</template>
+<script>
+export default {
+  destroyed() {
+    console.log("destroyes");
+  },
+  deactivated(){
+    console.log("deactivated");
+  },
+  activated(){
+    console.log("activated");
+  }
+}
+</script>
